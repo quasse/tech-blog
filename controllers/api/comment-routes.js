33008371238d2cec1784cd6ac13f9cd8aeab0d06
Sam Morgen <samorgen14@gmail.com>
2021-08-01 11:44:28 -0700

    comment model and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { Comment, User, Blog } = require("../../models");
+
+router.get("/", (req, res) => {
+  Comment.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Blog,
+        attributes: ["title"],
+      },
+    ],
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id, //will change to req.session.user_id
+    blog_id: req.body.blog_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Blog = require("./Blog");
+const Comment = require("./Comment");
 
 User.hasMany(Blog, {
   foreignKey: "user_id",
@@ -10,4 +11,23 @@ Blog.belongsTo(User, {
   onDelete: "SET NULL",
 });
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: "blog_id",
+  onDelete: "SET NULL",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+});
+
+module.exports = { User, Blog, Comment };
