526e642fc15e29f44e92c5b24816667dbf99dc6e
Sam Morgen <samorgen14@gmail.com>
2021-08-02 21:36:20 -0700

    edit and delete

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,4 +33,39 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/edit/:id", (req, res) => {
+  Blog.findByPk(req.params.id, {
+    attributes: ["Id", "blog_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (dbBlogData) {
+        const blog = dbBlogData.get({ plain: true });
+
+        res.render("edit-blog", {
+          blog,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
