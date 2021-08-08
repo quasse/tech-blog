cd7d13215e7565681617aa48fdf6957c392c260b
Sam Morgen <samorgen14@gmail.com>
2021-08-08 15:30:07 -0700

    Changed secret

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.DB_SECRET,
+  secret: "Secretpassword",
   cookie: {},
   resave: false,
   saveUninitialized: true,
